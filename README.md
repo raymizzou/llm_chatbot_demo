2af7e7385091d2ef5bc58ce548f96d2c67435221
ray <ray050899@gmail.com>
2024-04-04 13:13:29 +0800

    feat: update readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # llm_chatbot_demo
-demo project for AI chatbot
+
+This a demo python project for llm chatbot.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Documentation](#documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Provide instructions on how to install your project. Include any dependencies that need to be installed and how to install them.
+
+```bash
+pip3 install Flask
+pip3 install zhipuai
+```
+
+## Usage
+Using venv optional but recommended, 
+```commandline
+source ./venv/bin/activate
+```
+
+Start the server
+```commandline
+python3 access.py
+```
+
+## Documentation
+
+## Contributing
+
+## License
+
+
+
--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,18 @@
 import time
-import uuid
-
-# 导入所需的库和模块
 from zhipuai import ZhipuAI
 from logger import logger
 
 # add your zhipuai api key here
 ZHIPUAI_API_KEY = ''
 
-# 带记忆的聊天机器人类
+
 class BaseChatbot:
     def __init__(self):
         self.round = 0
         self.max_retries = 5
         self.delay = 2
         self.client = self.client = ZhipuAI(
-                api_key=ZHIPUAI_API_KEY,
+            api_key=ZHIPUAI_API_KEY,
         )
         self.llm = 'zhipu'
         self.model = 'glm-4'
@@ -28,39 +25,21 @@ class BaseChatbot:
 
         for attempt in range(self.max_retries):
             try:
-                if self.llm == 'openai':
-                    if request_id:
-                        response = self.client.chat.completions.create(
-                            model=self.model,  # 填写需要调用的模型名称
-                            messages=self.messages,
-                            n=1,
-                        )
-                        logger.info("response with request_id:%s", response)
-                        return self.assemble_openai_result(response, request_id)
-                    else:
-                        response = self.client.chat.completions.create(
-                            model=self.model,  # 填写需要调用的模型名称
-                            messages=self.messages,
-                            n=1
-                        )
-                        logger.info("response without request_id:%s", response)
-                        return self.assemble_openai_result(response)
+                if request_id:
+                    response = self.client.chat.completions.create(
+                        model=self.model,  # 填写需要调用的模型名称
+                        messages=self.messages,
+                        request_id=request_id,
+                    )
+                    logger.info("response with request_id:%s", response)
+                    return self.assemble_zhipu_result(response)
                 else:
-                    if request_id:
-                        response = self.client.chat.completions.create(
-                            model=self.model,  # 填写需要调用的模型名称
-                            messages=self.messages,
-                            request_id=request_id,
-                        )
-                        logger.info("response with request_id:%s", response)
-                        return self.assemble_zhipu_result(response)
-                    else:
-                        response = self.client.chat.completions.create(
-                            model=self.model,  # 填写需要调用的模型名称
-                            messages=self.messages,
-                        )
-                        logger.info("response without request_id:%s", response)
-                        return self.assemble_zhipu_result(response)
+                    response = self.client.chat.completions.create(
+                        model=self.model,  # 填写需要调用的模型名称
+                        messages=self.messages,
+                    )
+                    logger.info("response without request_id:%s", response)
+                    return self.assemble_zhipu_result(response)
             except Exception as e:
                 print(f"尝试 {attempt + 1}/{self.max_retries} 失败: {e}")
                 if attempt < self.max_retries - 1:  # 如果不是最后一次尝试，则延迟
@@ -80,21 +59,6 @@ class BaseChatbot:
         }
         return result
 
-    def assemble_openai_result(self, response, request_id=None):
-        ai_content = response.choices[0].message.content
-        self.messages.append({'role': 'assistant', 'content': ai_content})
-        result = {
-            "created": response.created,
-            "model": response.model,
-            "message": response.choices[0].message.content,
-            "round": self.round
-        }
-        if request_id:
-            result['request_id'] = request_id
-        else:
-            result['request_id'] = self.generate_uuid()
-        return result
-
     def assemble_error_result(self, request_id):
         result = {
             "created": int(time.time()),
@@ -104,15 +68,3 @@ class BaseChatbot:
             "round": self.round
         }
         return result
-
-    def generate_uuid(self):
-        # Generate a UUID
-        uuid_value = uuid.uuid4()
-
-        # Convert UUID to a string and remove dashes
-        uuid_str = str(uuid_value).replace("-", "")
-
-        # Convert hexadecimal UUID to decimal
-        uuid_decimal = int(uuid_str, 16)
-
-        return str(uuid_decimal)
